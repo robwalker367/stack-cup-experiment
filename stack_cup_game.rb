8d53bb791759f3ece9de5807f25a652695c396f9
Rob Walker <robertsrwalker@gmail.com>
2021-02-11 20:58:51 -0500

    Add more classes

--- stack_cup_game.rb
+++ stack_cup_game.rb
@@ -1,45 +1,85 @@
 require 'set'
 
-class PlayerCircle
-  attr_accessor :num_players, :p_players
+class StackCupGame
+  attr_accessor :players
+
+  TOTAL_CUPS = 2
 
   def initialize(**args)
-    @num_players = args[:num_players]
-    @p_players = args[:p_players]
+    @players = args[:players]
+    pass_out_cups
   end
 
-  def head
-    @head ||= begin
-      head = Node.new(players[0], nil)
-      prev_node = head
-      (1..num_players-1).each do |i|
-        prev_node.right = Node.new(players[i], nil)
-        prev_node = prev_node.right
-      end
-      prev_node.right = head
-      head
+  def pass_out_cups
+    players.at_position(0).holds_cup = true
+    players.at_position(players.size / TOTAL_CUPS).holds_cup = true
+  end
+end
+
+class Circle
+  attr_accessor :head, :size
+
+  def initialize(values: [])
+    @size = values.size
+    @head = arrange(values)
+  end
+
+  def arrange(values)
+    head = Node.new(values[0], nil)
+    prev_node = head
+    (1..values.size-1).each do |i|
+      prev_node.right = Node.new(values[i], nil)
+      prev_node = prev_node.right
     end
+    prev_node.right = head
+    head
   end
 
-  def players
-    @players ||= num_players.times.map { Player.new(p_players) }
+  def at_position(index)
+    node = head
+    index.times { node = node.right }
+    node
   end
 
-  Node = Struct.new(:player, :right)
+  Node = Struct.new(:value, :right)
+end
+
+class PlayersFactory
+  def self.create_players(total_players:, players_probability:)
+    total_players.times.map { 
+      Player.new(shot_probability: players_probability) 
+    }
+  end
 end
 
 class Player
-  attr_accessor :p_shot, :is_first_shot
-  def initialize(p_shot)
-    @p_shot = p_shot
+  attr_accessor :shot_probability, :is_first_shot, :holds_cup
+
+  def initialize(shot_probability:)
+    @shot_probability = shot_probability
     @is_first_shot = true
+    @holds_cup = false
+  end
+
+  def take_shot
+    is_first_shot = false if is_first_shot
+    (rand % shot_probability) < shot_probability
+  end
+
+  def pass_cup
+    holds_cup = false
+  end
+
+  def take_cup
+    holds_cup = true
+    is_first_shot = true
   end
 end
 
 class CircleUtil
-  def self.print_circle(circle_head)
+  def self.print_circle(circle)
     object_ids = Set.new
-    current_node = circle_head
+    current_node = circle.head
     while !object_ids.include?(current_node.object_id)
       puts current_node.object_id.to_s
       object_ids.add(current_node.object_id)
@@ -48,5 +88,10 @@ class CircleUtil
   end
 end
 
-c = PlayerCircle.new(num_players: 6, p_players: 0.5)
-CircleUtil.print_circle(c.head)
+players = Circle.new(
+  values: PlayersFactory.create_players(
+    total_players: 6,
+    players_probability: 0.5
+  )
+)
+CircleUtil.print_circle(players)
