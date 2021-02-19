4c61a0e0dbb4271a981fe00ac1287ff970be7c58
Rob Walker <robertsrwalker@gmail.com>
2021-02-18 20:35:23 -0500

    Refactor

--- stack_cup_game.rb
+++ stack_cup_game.rb
@@ -3,60 +3,37 @@ require 'set'
 class StackCupGame
   attr_accessor :players
 
-  TOTAL_CUPS = 2
-
   def initialize(**args)
     @players = args[:players]
-    pass_out_cups
-  end
-
-  def pass_out_cups
-    players.at_position(0).holds_cup = true
-    players.at_position(players.size / TOTAL_CUPS).holds_cup = true
   end
 end
 
-class Circle
-  attr_accessor :head, :size
+class Cups
+  attr_accessor :players_with_cups
 
-  def initialize(values: [])
-    @size = values.size
-    @head = arrange(values)
+  def initialize(player_circle:)
+    @player_circle = player_circle
   end
 
-  def arrange(values)
-    head = Node.new(values[0], nil)
-    prev_node = head
-    (1..values.size-1).each do |i|
-      prev_node.right = Node.new(values[i], nil)
-      prev_node = prev_node.right
+  def distribute_cups
+    indices_of_cups = [0, player_circle.total_players / 2]
+    indices_of_cups.each do |i|
+      players_with_cups.append(player_circle.player_at_index(i))
     end
-    prev_node.right = head
-    head
   end
 
-  def at_position(index)
-    node = head
-    index.times { node = node.right }
-    node
-  end
-
-  Node = Struct.new(:value, :right)
-end
-
-class PlayersFactory
-  def self.create_players(total_players:, players_probability:)
-    total_players.times.map { 
-      Player.new(shot_probability: players_probability) 
-    }
+  def pass_to_player(player_with_cup, player_to_pass_to)
+    players_with_cups[players_with_cups.index(player_with_cup)] = player_to_pass_to
   end
 end
 
 class Player
-  attr_accessor :shot_probability, :is_first_shot, :holds_cup
+  attr_accessor :shot_probability, :is_first_shot, :right_player, :left_player
 
   def initialize(shot_probability:)
     @shot_probability = shot_probability
+    @right_player = nil
+    @left_player = nil
     @is_first_shot = true
     @holds_cup = false
   end
@@ -65,33 +42,65 @@ class Player
     is_first_shot = false if is_first_shot
     (rand % shot_probability) < shot_probability
   end
+end
 
-  def pass_cup
-    holds_cup = false
+class PlayerCircle
+  attr_accessor :total_players, :players
+
+  def initialize(players: [])
+    @total_players = players.size
+    @players = players
   end
 
-  def take_cup
-    holds_cup = true
-    is_first_shot = true
+  def head
+    @head ||= begin
+      first_player = players[0]
+      prev_player = first_player
+      (total_players-1).times do |i|
+        next_player = players[i+1]
+        prev_player.right_player = next_player
+        next_player.left_player = prev_player
+        prev_player = next_player
+      end
+      prev_player.right_player = first_player
+      first_player.left_player = prev_player
+      first_player
+    end
+  end
+
+  def player_at_index(index)
+    player = head
+    index.times do
+      player = player.right_player
+    end
+    player
   end
 end
 
-class CircleUtil
-  def self.print_circle(circle)
+class PlayerFactory
+  def self.create_players(total_players:, players_probability:)
+    total_players.times.map { 
+      Player.new(shot_probability: players_probability) 
+    }
+  end
+end
+
+class Util
+  def self.print_players(players)
     object_ids = Set.new
-    current_node = circle.head
+    current_node = players.head
     while !object_ids.include?(current_node.object_id)
       puts current_node.object_id.to_s
       object_ids.add(current_node.object_id)
-      current_node = current_node.right
+      current_node = current_node.right_player
     end
   end
 end
 
-players = Circle.new(
-  values: PlayersFactory.create_players(
+players = PlayerCircle.new(
+  players: PlayerFactory.create_players(
     total_players: 6,
     players_probability: 0.5
   )
 )
-CircleUtil.print_circle(players)
+Util.print_players(players)
