436fa3f63558cbbccbf6dead1e7d06f4bf25f4f6
Rob Walker <robertsrwalker@gmail.com>
2021-02-18 20:51:32 -0500

    More work

--- stack_cup_game.rb
+++ stack_cup_game.rb
@@ -1,45 +1,67 @@
 require 'set'
 
 class StackCupGame
-  attr_accessor :players
+  attr_accessor :player_circle, :cups
 
   def initialize(**args)
-    @players = args[:players]
+    @player_circle = args[:player_circle]
+    @cups = args[:cups]
+  end
+
+  def rounds_until_first_stack
+    cups.distribute_cups(player_circle)
+    round_count = 0
+    while !cups.cup_was_stacked?
+      do_round
+      round_count += 1
+    end
+    round_count
+  end
+
+  def do_round
+    cups.players_with_cups.each do |player|
+      is_success = player.made_shot?
+      if is_success
+        cups.pass_cup(player, player.right_player)
+      end
+    end
   end
 end
 
 class Cups
-  attr_accessor :players_with_cups
+  attr_accessor :players_with_cups, :total_cups
 
-  def initialize(player_circle:)
-    @player_circle = player_circle
+  def initialize(total_cups:)
+    @total_cups = total_cups
   end
 
-  def distribute_cups
-    indices_of_cups = [0, player_circle.total_players / 2]
+  def distribute_cups(player_circle)
+    indices_of_cups = [0, player_circle.total_players / total_cups]
     indices_of_cups.each do |i|
       players_with_cups.append(player_circle.player_at_index(i))
     end
   end
 
-  def pass_to_player(player_with_cup, player_to_pass_to)
+  def pass_cup(player_with_cup, player_to_pass_to)
     players_with_cups[players_with_cups.index(player_with_cup)] = player_to_pass_to
+  end
+
+  def cup_was_stacked?
+    players_with_cups.size < total_cups
   end
 end
 
 class Player
-  attr_accessor :shot_probability, :is_first_shot, :right_player, :left_player
+  attr_accessor :shot_probability, :right_player, :left_player
 
   def initialize(shot_probability:)
     @shot_probability = shot_probability
     @right_player = nil
     @left_player = nil
-    @is_first_shot = true
     @holds_cup = false
   end
 
-  def take_shot
-    is_first_shot = false if is_first_shot
+  def made_shot?
     (rand % shot_probability) < shot_probability
   end
 end
@@ -97,10 +119,16 @@ class Util
   end
 end
 
-players = PlayerCircle.new(
+player_circle = PlayerCircle.new(
   players: PlayerFactory.create_players(
     total_players: 6,
     players_probability: 0.5
   )
 )
-Util.print_players(players)
+
+game = StackCupGame.new(
+  player_circle: player_circle,
+  cups: Cups.new(total_cups: 2)
+)
+
+puts game.rounds_until_first_stack
