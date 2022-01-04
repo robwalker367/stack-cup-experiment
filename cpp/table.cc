2bfa2284ea53ecb2c5d0b8c65af16c425d627cce
Rob Walker <robertsrwalker@gmail.com>
2022-01-03 21:53:44 -0500

    Refactor

--- cpp/table.cc
+++ cpp/table.cc
@@ -10,29 +10,29 @@ stack_table::stack_table(int _player_count, int _middle_cups)
 stack_table::~stack_table() {
 }
 
-stack_player* stack_table::player(int position) {
-  return &this->players[position % this->player_count];
+stack_player& stack_table::player(int position) {
+  return this->players[position % this->player_count];
 }
 
 int stack_cup::move() {
-  this->table.mutex.lock();
+  std::scoped_lock guard(this->table.mutex);
   bool made_shot = random_int(0, 1);
   if (made_shot) {
-    stack_player* next_player = this->table.player(this->position + 1);
-    if (next_player->cup) {
+    stack_player& next_player = this->table.player(this->position + 1);
+    if (next_player.cup) {
       // Stack the cup onto the next player's cup
-      this->player()->cup = nullptr;
-      next_player->cup->stacks += this->stacks;
+      this->player().cup = nullptr;
+      next_player.cup->stacks += this->stacks;
 
       // Next player passes stacked cup to next player without a cup
       int i = this->position + 2;
-      while (this->table.player(i)->cup) {
+      while (this->table.player(i).cup) {
         ++i;
       }
-      assert(!this->table.player(i)->cup);
-      this->table.player(i)->cup = next_player->cup;
-      this->table.player(i)->cup->position = i % this->table.player_count;
-      next_player->cup = nullptr;
+      assert(!this->table.player(i).cup);
+      this->table.player(i).cup = next_player.cup;
+      this->table.player(i).cup->position = i % this->table.player_count;
+      next_player.cup = nullptr;
 
       // Next player takes a cup from the middle
       --this->table.middle_cups;
@@ -40,25 +40,22 @@ int stack_cup::move() {
         // Game ends
         exit(0);
       }
-      next_player->cup = this;
+      next_player.cup = this;
       ++this->position;
       this->stacks = 1;
-      this->table.mutex.unlock();
       return -1;
     } else {
       // Pass cup to next player
-      this->player()->cup = nullptr;
-      next_player->cup = this;
+      this->player().cup = nullptr;
+      next_player.cup = this;
       ++this->position;
-      this->table.mutex.unlock();
       return 1;
     }
   } else {
-    this->table.mutex.unlock();
     return 0;
   }
 }
 
-stack_player* stack_cup::player() {
+stack_player& stack_cup::player() {
   return this->table.player(this->position);
 }
--- cpp/table.hh
+++ cpp/table.hh
@@ -5,33 +5,31 @@ struct stack_player;
 struct stack_cup;
 
 struct stack_table {
-  std::mutex mutex;
   int player_count;
   int middle_cups;
+  std::mutex mutex;
   std::vector<stack_player> players;
 
   stack_table(int _player_count, int _middle_cups);
   ~stack_table();
 
-  stack_player* player(int position);
+  stack_player& player(int position);
 };
 
 struct stack_player {
   stack_cup* cup = nullptr;
-  bool is_first_shot = true;
 };
 
 struct stack_cup {
   stack_table& table;
   int position = 0;
   int stacks = 1;
-  bool stopped = false;
 
   stack_cup(stack_table& _table, int _position)
     : table(_table), position(_position) {
   }
 
-  stack_player* player();
+  stack_player& player();
 
   int move();
 };
